0c0fddd83f45d610d606838ec2d17a668ddcf1b6
Hannanah Mobashir <hanana.m@gmail.com>
2017-09-14 10:47:09 -0400

    Update reverseString.py

--- reverseString.py
+++ reverseString.py
@@ -1,9 +1,25 @@
 # Problem prompt:
 # Write a function that takes a string as input and returns the string reversed.
+
+# this solution timed out
+# def reverseString(s):
+# 	new_s = ''
+# 	if s:
+# 		for i in range(0, len(s)):
+# 			new_s += s[len(s)-i-1]
+# 		return new_s
+# 	return s 
+
 def reverseString(s):
- 	new_s = ''
- 	if s:
- 		for i in range(0, len(s)):
- 			new_s += s[len(s)-i-1]
- 		return new_s
- 	return s 
+	s = list(s)
+	for i in range(0, len(s)/2):
+		temp = s[i]
+		s[i] = s[len(s)-1-i]
+		s[len(s)-1-i] = temp
+	return str(''.join(s))
+
+# test cases
+print 'hello' + ' reversed is ' + reverseString('hello')
+print '' + ' reversed is ' + reverseString('')
+print 'hel lo' + ' reversed is ' + reverseString('hel lo')
+print 'this string will end up getting reversed' + ' reversed is ' + reverseString('this string will end up getting reversed')
