687dab205cef06ef2e65e3019c9813b922017428
Hannanah Mobashir <hanana.m@gmail.com>
2017-10-04 20:20:03 -0400

    fix: fixed timeout
    fixed timeout with a quicker and simpler solution

--- singleNumber.py
+++ singleNumber.py
@@ -3,15 +3,13 @@
 # Find that single one.
 
 def singleNumber(nums):
-	for n in nums:
-		if nums.count(n) == 1:
-			return n
+	output = 2*sum(set(nums)) - sum(nums)
+	return output
 
 # test cases
 numbers = [[1,2,3,1,4,2,3],
-			[99,99,0,7,6,4,3,4,3,7,6],
-			[0,0,1]
-			]
+	[99,99,0,7,6,4,3,4,3,7,6],
+	[0,0,1]]
 
 for i in range(len(numbers)):
 	print singleNumber(numbers[i])
